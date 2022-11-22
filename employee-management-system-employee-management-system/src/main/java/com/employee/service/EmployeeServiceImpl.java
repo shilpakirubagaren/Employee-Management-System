bc9ad97123f97c53a8f990ce728f96f7054d4a0e
shilpakirubagaren <115527908+shilpakirubagaren@users.noreply.github.com>
2022-11-22 12:42:25 +0530

    Add files via upload

---
+++ employee-management-system-employee-management-system/src/main/java/com/employee/service/EmployeeServiceImpl.java
@@ -0,0 +1,46 @@
+package com.employee.service;
+
+import com.employee.model.Employee;
+import com.employee.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public List<Employee> getAllEmployee() {
+        return employeeRepository.findAll();
+    }
+
+    public void save(Employee employee) {
+        if(Objects.nonNull(employee)){
+            employeeRepository.save(employee);
+        }
+    }
+
+    public Employee getById(Long id) {
+        Employee employee = null;
+        if (Objects.nonNull(id)) {
+            Optional<Employee> optionalEmployee = employeeRepository.findById(id);
+            if(optionalEmployee.isPresent()){
+                employee = optionalEmployee.get();
+            }else{
+                throw new RuntimeException("Employee not found with the id:"+ id);
+            }
+        }
+        return employee;
+    }
+
+    public void deleteById(Long id) {
+        if(Objects.nonNull(id)){
+            employeeRepository.deleteById(id);
+        }
+    }
+}
